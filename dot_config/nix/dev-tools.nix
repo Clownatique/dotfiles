077d6662ad7f5749b7c4164fe147f322d724d64a
Clownatique <132279628+Clownatique@users.noreply.github.com>
2026-07-25 12:51:37 +0200

    feat(language): add languages in some configs - language support in helix conf - lsp add in nix files

--- dot_config/nix/dev-tools.nix
+++ dot_config/nix/dev-tools.nix
@@ -3,29 +3,34 @@
 {
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+  nixpkgs.config.permittedInsecurePackages = [
+      "pnpm-9.15.9"
+      "quickjs-2025-09-13-2"
+  ];
   environment.systemPackages = with pkgs; [
+     wev
+     unzip
      git
-     gcc
-     clang_20
-     ocaml
      chezmoi
      usbutils
-     emmet-language-server
-     clang-tools
-     ocamlPackages.lsp
-     racket
+     oh-my-zsh
+     # emmet-language-server
+     # clang-tools
+     # ocamlPackages.lsp
+     # racket
      taplo
      marksman
      nixd
-     vscode-css-languageserver
-     superhtml
-     vscode-json-languageserver
-     emmet-ls
-     python315
-     luaformatter
-     prettier
-     jinja-lsp
-     lua-language-server
+     # vscode-css-languageserver
+     # superhtml
+     # vscode-json-languageserver
+     # emmet-ls
+     # python315
+     # luaformatter
+     # prettier
+     # jinja-lsp
+     # lua-language-server
+     tic-80
   ];
   programs.tmux = {
     enable = true;
